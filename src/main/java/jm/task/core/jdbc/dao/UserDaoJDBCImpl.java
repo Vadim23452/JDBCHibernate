64254173ed170becad7946ba72a5ee5c8a7ae7c9
vdim <vadim23452@yandex.ru>
2023-11-21 13:53:16 +0300

    delete target and .idea

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,7 @@
 package jm.task.core.jdbc.dao;
 
-import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
+import jm.task.core.jdbc.model.User;
 
 import java.sql.*;
 import java.util.ArrayList;
